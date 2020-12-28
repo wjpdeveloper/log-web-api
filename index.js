963d4063d164d191910ba7324b8cd063bcd37160
Courthasl Zhao <chunsen.zhao@navercorp.com>
2020-12-28 18:03:16 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const fs = require('fs')
+const path = require('path')
+const glob = require('glob')
+const app = express()
+
+app.all('*', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With')
+  res.header('Access-Control-Allow-Methods','PUT,POST,GET,DELETE,OPTIONS')
+  res.header('X-Powered-By',' 3.2.1')
+  res.header('Content-Type', 'application/json;charset=utf-8')
+  next()
+})
+
+const remotePath = '/Users/courthasl/Documents/test/'
+const fileArr = []
+
+const getDirectories = function (src, callback) {
+  glob(src + '/**/*', callback)
+}
+
+app.get('/', function (req, res) {
+  getDirectories(remotePath, function (err, directories) {
+    if (err) throw err
+    console.log(directories)
+    res.send(directories.map(r => r.replace(remotePath, '')))
+  })
+})
+app.get('/log', function (req, res) {
+  const src = req.query.src
+  const filePath = path.join(remotePath, src)
+  fs.readFile(filePath, 'utf8', function (err, data) {
+    console.log(data)
+    res.send(data)
+  })
+})
+
+app.listen(9998)
